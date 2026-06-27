702f3b9e186299c34acc60327c06a30b85637b53
chariri <w@chariri.moe>
2026-06-28 02:40:04 +0900

    fix(api): guard missing trial workflow response

--- api/controllers/console/explore/trial.py
+++ api/controllers/console/explore/trial.py
@@ -800,6 +800,9 @@ class AppWorkflowApi(Resource):
             raise AppUnavailableError()
 
         workflow = db.session.get(Workflow, app_model.workflow_id)
+        if workflow is None:
+            raise AppUnavailableError()
+
         return dump_response(TrialWorkflowResponse, workflow)
 
 
